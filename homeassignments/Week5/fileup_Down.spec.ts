45e2af541739b8e0720f724a08fd420c47a20937
asksakthe <asksakthe@outlook.com>
2024-08-15 16:10:14 +0530

    fileupload excescise

---
+++ homeassignments/Week5/fileup_Down.spec.ts
@@ -0,0 +1,24 @@
+import {test, expect} from '@playwright/test'
+import path from 'path'
+
+
+test('upload a file',async ({page}) => {
+    await page.goto("https://the-internet.herokuapp.com/upload")
+
+    await page.locator("#file-upload").locator("input[type='file']").setInputFiles([path.join(__dirname,"feb2024.jpeg)]")])
+    
+    await expect(page.locator("#uploaded-files")).toContainText("feb2024")
+}
+)
+
+test.only('Dwld a targetFile',async ({page}) => {
+
+    await page.goto("https://the-internet.herokuapp.com/download") 
+    const fileDw01 = page.waitForEvent('download')
+    await page.locator("//a[text()='students.json']").click()
+    const fileDwld = await fileDw01
+
+    await fileDwld.saveAs(path.join("downloads/"+fileDwld.suggestedFilename()))
+    await page.waitForTimeout(5000)
+}
+)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
